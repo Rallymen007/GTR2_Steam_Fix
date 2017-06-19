7fc67cde5868978ab50cd8ba337013532f7d96f7
Franck Dechavanne <dechavanne.franck@hotmail.fr>
2017-06-19 13:31:58 +0200

    Initial commit

---
+++ GTR2LauncherExe/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GTR2LauncherExe {
+    class Program {
+        static void Main(string[] args) {
+            ProcessStartInfo psi = new ProcessStartInfo("GTR2.exe") {
+
+            };
+            Process p = Process.Start(psi);
+            p.WaitForExit();
+        }
+    }
+}
